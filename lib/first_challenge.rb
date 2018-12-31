8fe901082a9c90b287ffe485935d62693479827a
powell956 <powell956@gmail.com>
2018-12-31 04:41:04 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -17,11 +17,10 @@ def first_challenge
   contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
   contacts["Freddie Mercury"].each do |data, value|
     if data == favorite_icecream_flavors
-      value.delete_if do |flavor| 
+      value.delete_if do |flavor|
         flavor == "strawberry"
       end
   end
   #remember to return your newly altered contacts hash!
   contacts
 end
-
